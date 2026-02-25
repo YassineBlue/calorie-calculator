e0a9aad051f5335cfe26fd1053181b399e051c81
YassineBlue <yassineyassar1@gmail.com>
2026-02-25 23:35:40 +0000

    Move JS to external file for clarity

---
+++ script.js
@@ -0,0 +1,21 @@
+ document.querySelector('#Calculer').addEventListener('click',function(){
+            let tkcalviande = parseFloat(document.querySelector('#Viande').value) * 2.5 ;
+            let tkcalpain = parseFloat(document.querySelector('#Pain').value) * 2.65 ;
+            let tkcalLegume = parseFloat(document.querySelector('#Légumes').value) * .5 ;
+            let tkcalFruits = parseFloat(document.querySelector('#Fruits').value) * .6 ;
+            let tkcalYaourt = parseFloat(document.querySelector('#Yaourt').value) * .8 ;
+            let tkcal = tkcalviande + tkcalpain + tkcalLegume + tkcalFruits + tkcalYaourt ; //total 
+            if(tkcal<500){
+                document.querySelector('#result').textContent=`Total cals: ${tkcal} Kcals , Repas léger`;
+                document.querySelector('#result').style.color='orange';
+            }else{
+                if(tkcal<800){
+                    document.querySelector('#result').textContent=`Total cals: ${tkcal} Kcals , Repas équilibré`;
+                    document.querySelector('#result').style.borderColor='green';
+
+                }else{
+                    document.querySelector('#result').textContent=`Total cals: ${tkcal} Kcals , Repas riche en calories`;
+                    document.querySelector('#result').style.borderColor='red';
+                }
+            }
+            });
