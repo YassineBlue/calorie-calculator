3af8d0765988a73b6aad2d4b2184309b4a7f4779
YassineBlue <yassineyassar1@gmail.com>
2026-02-26 02:24:31 +0000

    Improved JS to handle more scenarios

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
  document.querySelector('#Calculer').addEventListener('click',function(){
-            let tkcalviande = parseFloat(document.querySelector('#Viande').value) * 2.5 ;
-            let tkcalpain = parseFloat(document.querySelector('#Pain').value) * 2.65 ;
-            let tkcalLegume = parseFloat(document.querySelector('#Légumes').value) * .5 ;
-            let tkcalFruits = parseFloat(document.querySelector('#Fruits').value) * .6 ;
-            let tkcalYaourt = parseFloat(document.querySelector('#Yaourt').value) * .8 ;
-            let tkcal = tkcalviande + tkcalpain + tkcalLegume + tkcalFruits + tkcalYaourt ; //total
-                tkcal = Number(tkcal);
-            if(isNaN(tkcal)){
-             alert("Please Enter only numbers ");
-             return ;
+            let tkcalviande = (parseFloat(document.querySelector('#Viande').value)|| 0) * 2.5;
+            let tkcalpain =   (parseFloat(document.querySelector('#Pain').value) || 0) * 2.65;
+            let tkcalLegume = (parseFloat(document.querySelector('#Légumes').value) || 0) * .5;
+            let tkcalFruits = (parseFloat(document.querySelector('#Fruits').value)|| 0) * .6;
+            let tkcalYaourt = (parseFloat(document.querySelector('#Yaourt').value)|| 0 ) * .8;
+            //total
+            let tkcal = tkcalviande + tkcalpain + tkcalLegume + tkcalFruits + tkcalYaourt ; 
+            // case of empty inputs
+            if(tkcal === 0){
+             alert("Pour calculer la valeur énergétique, merci d’entrer au moins la quantité consommée d’un aliment");
+             return ; 
             }
+            let result = document.querySelector('#result') ;
+            // meal is light
             if(tkcal<500){
-                document.querySelector('#result').textContent=`Total cals: ${tkcal} Kcals , Repas léger`;
-                document.querySelector('#result').style.color='orange';
-            }else{
-                if(tkcal<800){
-                    document.querySelector('#result').textContent=`Total cals: ${tkcal} Kcals , Repas équilibré`;
-                    document.querySelector('#result').style.color='green';
-
-                }else{
-                    document.querySelector('#result').textContent=`Total cals: ${tkcal} Kcals , Repas riche en calories`;
-                    document.querySelector('#result').style.color='red';
-                }
+                result.textContent=`Total cals: ${tkcal} Kcals , Repas léger`;
+                result.style.color='orange';
             }
+            // meal is balanced 
+            else if(tkcal<800){
+                    result.textContent=`Total cals: ${tkcal} Kcals , Repas équilibré`;
+                    result.style.color='green';
+                }
+            // meal is high in calories
+            else{
+                    result.textContent=`Total cals: ${tkcal} Kcals , Repas riche en calories`;
+                    result.style.color='red';
+                }
             });
